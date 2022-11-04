39aeee90632dd1b2e1f1aafe85b67a5988101b7d
Mango <fengmango@outlook.com>
2022-11-04 14:46:07 +0800

    Update pmatrix-related calculation

--- mango_python/ct.py
+++ mango_python/ct.py
@@ -38,7 +38,31 @@ def norm_pmatrix(pmatrix: np.ndarray, du: float = None) -> np.ndarray:
     return pmatrix / lamda
 
 
-def calc_pmatrix(points_xyz: np.ndarray, points_uv: np.ndarray, du: float = None) -> np.ndarray:
+def parse_pmatrix(pmatrix: np.ndarray, du: float = None) -> tuple:
+
+    if du is not None:
+        pmatrix = norm_pmatrix(pmatrix)
+
+    # Matrix Q
+    qmatrix = np.linalg.inv(pmatrix[:, 0:3])
+
+    # source-origin distance
+    r_s = -qmatrix @ pmatrix[:,3]
+    sod = np.linalg.norm(r_s)
+
+    # source-detector distance
+    e_u = qmatrix[:,0]
+    e_v = qmatrix[:,1]
+    r_sd = qmatrix[:,2]
+
+    n = np.cross(e_u, e_v)
+    sdd = np.abs(np.dot(r_sd, n)) / np.linalg.norm(n)
+
+    return sod, sdd
+
+
+
+def fit_pmatrix(points_xyz: np.ndarray, points_uv: np.ndarray, du: float = None) -> np.ndarray:
     """Calculate p-matrix with given points of (x,y,z) and (u,v)
 
     Parameters
@@ -95,6 +119,59 @@ def calc_pmatrix(points_xyz: np.ndarray, points_uv: np.ndarray, du: float = None
     return pmatrix
 
 
+def calc_pmatrix_analytical(sod: float, sdd: float, du: float, u_shift: float = 0, v_shift: float = 0, alpha: float = 0, beta: float = 0, gamma: float = 0, theta: float = 0, phi: float = 0) -> np.ndarray:
+    # TODO: add comment
+    # calculate p-matrix analytically based on given geometry description
+    # sod: source-origin distance [mm]
+    # sdd: source-detector distance [mm]
+    # du: detector element size [mm]
+    # u_shift: detector shift distance along u direction [mm]
+    # v_shift: detector shift distance along v direction [mm]
+    # alpha, beta, gamma: rotation angle of detector along x, y and z axis, respectively [radius]
+    # theta, phi: angle of source described in a custom spherical coordinate system (phi starts from -y direction) [radius]
+
+    # Instead of calculating Q=Rz·Ry·Rx·Q0 followed by calculating Q_inv, we directly calculate Q_inv with equation:
+    #    Q_inv = Q0_inv · Rx_inv · Ry_inv · Rz_inv
+
+    # Matrix of Q0 (just a demonstration)
+    # Q0 = np.array([[du, 0,  u_shift],
+    #                [0,  0,  sdd],
+    #                [0,  du, v_shift]])
+
+    # Matrix of Q0 inverse
+    Q0_inv = np.array([[1/du, -u_shift/(du*sdd),    0],
+                       [   0, -v_shift/(du*sdd), 1/du],
+                       [   0,             1/sdd,    0]])
+    
+    # Rotation matrix Rx, Ry, Rz inverse
+    Rx_inv = np.array([[            1,              0,               0],
+                       [            0,  np.cos(alpha),   np.sin(alpha)],
+                       [            0, -np.sin(alpha),   np.cos(alpha)]])
+
+    Ry_inv = np.array([[ np.cos(beta),              0,   -np.sin(beta)],
+                       [            0,              1,               0],
+                       [ np.sin(beta),              0,    np.cos(beta)]])
+
+    Rz_inv = np.array([[ np.cos(gamma),  np.sin(gamma),               0],
+                       [-np.sin(gamma),  np.cos(gamma),               0],
+                       [             0,              0,               1]])
+
+    # Calculate Q_inv = Q0_inv · Rx_inv · Ry_inv · Rz_inv
+    Q_inv = Q0_inv @ Rx_inv @ Ry_inv @ Rz_inv
+
+    # source position
+    xs =  sod * np.cos(theta) * np.sin(phi)
+    ys = -sod * np.cos(theta) * np.cos(phi)
+    zs =  sod * np.sin(theta)
+
+    # final p-matrix
+    pmatrix = Q_inv @ np.array([[1, 0, 0, -xs],
+                                [0, 1, 0, -ys],
+                                [0, 0, 1, -zs]])
+
+    return pmatrix
+
+
 def calc_uv_from_xyz_pmatrix(points_xyz: np.ndarray, pmatrix: np.ndarray) -> np.ndarray:
     """P-matrix formward projection. Calculate (u,v) from (x,y,z) using p-matrix.
 
@@ -129,7 +206,7 @@ def calc_uv_from_xyz_pmatrix(points_xyz: np.ndarray, pmatrix: np.ndarray) -> np.
     # Build matrix on the right-hand side (x,y,z,1)
     X = np.concatenate((points_xyz, np.ones((n,1))), axis=1).transpose()
     
-    # Calculate matrix U (alpha u, alpha v, alpha)
+    # Calculate matrix U (alpha*u, alpha*v, alpha)
     U = pmatrix @ X
     
     points_uv = U[0:2, :] / U[2, :]
